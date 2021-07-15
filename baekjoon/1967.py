c1856ad502170a35ca49cd967773e0f440186f62
yein-lee <agop0310@gmail.com>
2021-07-15 14:41:38 +0900

    .

--- baekjoon/1967.py
+++ baekjoon/1967.py
@@ -1,4 +1,5 @@
 import sys
+sys.setrecursionlimit(10**9)
 
 n = int(sys.stdin.readline())
 tree = {}
@@ -10,29 +11,33 @@ for i in range(n-1):
     else:
         tree[p] = [(c,cost)]
 
+    if c in tree:
+        tree[c].append((p,cost))
+    else:
+        tree[c] = [(p,cost)]
 
-for child in tree[1]:
-    print(child)
-    print(child[0])
-    print(child[1])
+length = [0 for _ in range(n+1)]
 
-
-longest = 0
 def dfs(v):
-    while(1):
-        if v not in tree:
-            break
-        max_cost = 0
-        for child in tree[v]:
-            if child[1]>max_cost:
-                max_node = child[0]
-                max_cost = child[1]
-        v=max_node
-        print(max_node, end=' ')
-        global longest
-        longest += max_cost
+    if v not in tree:
+        # print("%d not in tree" %v)
+        return
+    for child in tree[v]:
+        if length[child[0]]==0:
+            length[child[0]] = length[v] + child[1]
+            dfs(child[0])
 
-
-print(tree)
 dfs(1)
-print(longest)
+length[1]=0
+deepest_node = length.index(max(length))
+print(length)
+print(max(length))
+print(deepest_node)
+for i in range(len(length)):
+    length[i]=0
+print(length)
+dfs(deepest_node)
+length[deepest_node]=0
+print(max(length))
+
+print(length)
